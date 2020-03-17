f632211cce6a85122817f3d370c6b4cb9d7d7e81
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-17 18:21:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  put " #{board[0]} , #{board[1]} , #{board[2]} , #{board[3]} , #{board[4] , #{board[5]} , #{board[6]} , #{board[7] , #{board[8]} "
+  put " #{board[0]} , #{board[1]} , #{board[2]} , #{board[3]} , #{board[4]} , #{board[5]} , #{board[6]} , #{board[7]} , #{board[8]} "
 end
